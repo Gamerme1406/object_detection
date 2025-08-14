2c515c7f5508f89cacb25160b059fff598449036
Arnav <arnav.a1406@gmail.com>
2025-08-14 22:57:36 +0530

    requirments added

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # object_detection
+
+## How to run:
+
+```bash
+conda create -n objectdet python=3.11 -y
+```
+
+
+```bash
+conda activate objectdet
+```
+
+```bash
+pip install -r requirments.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='objectdet',
+    version='0.0.0',  
+    author='Arnav',  
+    author_email='arnav.a1406@gmail.com',  
+    packages=find_packages(),
+    install_requires=[] 
+)
